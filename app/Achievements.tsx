302a9b43efcdda4f83fc8953a305531a912dfec3
Tyrah <musiliuoyinkansolatyrah@gmail.com>
2026-07-15 11:07:42 -0700

    Added Icons to the Achievement Section

--- app/Achievements.tsx
+++ app/Achievements.tsx
@@ -1,15 +1,24 @@
+import { Space_Mono } from "next/font/google";
 import styles from "./components/Achievements.module.css";
+import {
+  LuGauge,
+  LuCircleCheckBig,
+  LuNetwork,
+} from "react-icons/lu";
 
 const achievements = [
     {
+        icon: <LuGauge />,
         title: "Performance Optimization",
         description: "Improved application performance, increasing load speeds by up to 40%",
     },
     {
+        icon: <LuCircleCheckBig />,
         title: "Production-Ready Deliveries",
         description: "Delivered multiple robust, production-ready web applications from concept to deployment.",
     },
     {
+        icon: <LuNetwork />,
         title: "Scaled Architecture",
         description: "Scaled applications to seamlessly support 20,000+ active users.",
     },
@@ -30,6 +39,7 @@ export default function Achievements () {
                 {achievements.map((achievement) => (
                     <article className={styles.achievementCard}>
                         <div className={styles.achievementsInfo}>
+                            {achievement.icon}
                             <h3>{achievement.title}</h3>
                             <p>{achievement.description}</p>
                         </div>
